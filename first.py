f27a7dbaff759c674f3c1c8a82c650d0906f395a
Mohit Kumar <mk2174334@gmail.com>
2026-09-02 09:41:37 +0530

    Python basics

---
+++ first.py
@@ -0,0 +1,144 @@
+# print("Hello Mohit")
+# print("I am learing python")
+# print("Start coding")
+
+# name = "Mohit"
+# print(name)
+
+# age = 23
+# print(age)
+
+# course = "MCA"
+# print(course)
+
+# a = 10
+# b = 20
+
+# print(a + b)
+
+# city = "Mumbai"
+
+# print(city)
+
+# x = 50
+# y = 30
+
+# print(x - y)
+
+# name = input("Enter your name:")
+
+# print(name)
+
+# name = input("Enter name")
+
+# print("Welcome",name)
+
+# name = input("Enter name")
+# city = input("Enter city")
+
+# print("Name",name)
+# print("City",city)
+
+# a = int(input("Enter first number:"))
+# b = int(input("Enter second number:"))
+
+# print(a + b)
+
+# num1 = int(input("Enter first number:"))
+# num2 = int(input("Enter second number:"))
+
+# print("Addition =",num1 + num2)
+# print("Subtraction =",num1 - num2)
+# print("Multiplication =",num1 * num2)
+# print("Division =",num1 / num2)
+
+# a = 10
+
+# print(a)
+# print(type(a))
+
+# b = 20
+
+# print(id(b))
+
+# if statement
+
+# age = 20
+
+# if age >= 18:
+#  print("Eligible")
+
+# if else statement
+
+# age = 15
+
+# if age>= 18:
+#     print("Eligible")
+# else: print("Minor")    
+
+# marks = 85
+
+# if marks >= 90:
+#     print("A")
+# elif marks >= 75:
+#     print("B")
+# elif marks >= 60:
+#     print("C")
+# else: print("Fail")            
+
+# Nested if
+
+# age = 20
+# citizen = True
+
+# if age >= 18:
+#     if citizen:
+#         print("Eligible")
+
+# for loop
+
+# for i in range(5):
+#     print(i) 
+
+# while loop
+
+# i = 1
+
+# while i <= 5:
+#     print(i)
+#     i += 1
+
+# break statement
+
+# for i in range(10):
+#     if i == 5:
+#         break
+#     print(i)
+
+# continue statement
+
+# for i in range(5):
+#     if i ==2:
+#         continue
+#     print(i)
+
+# pass statement
+
+# for i in range(5):
+#     if i == 3:
+#         pass
+#     print(i)
+
+# print("P" in "Python")
+# print("A" in "Python")
+
+# text = "Python"
+# print(len(text))
+
+# marks = [10,20,30,40]
+
+# print(marks[0])
+# print(marks[1])
+
+# a = (1,2)
+# print(a*2)
