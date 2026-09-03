835721a115daf7895aa8781d9c9ce2d31140181a
Mohit Kumar <mk2174334@gmail.com>
2026-09-03 09:39:38 +0530

    Practice Start

---
+++ practice.py
@@ -0,0 +1,39 @@
+print("Hello")
+
+name = "Mohit"
+print(name)
+
+age = 23
+print(age)
+
+course = "MCA"
+print(course)
+
+a = 10
+b = 20
+print(a + b)
+
+city = "Delhi"
+print(city)
+
+length = 4
+breath = 3
+print(length * breath)
+
+# name = input("Enter your name: ")
+
+# print(name)
+
+# name = input("Enter your name: ")
+
+# print("Welcome", name)
+
+# city = input("Enter your city:")
+# print(city)
+
+# a = int(input("Enter the number: "))
+
+# print(a)
+
+b = input("Enter your number: ")
+print(b)
