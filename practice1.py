64a7b26758839e3836d15015fb6f8f6d8e7a52d5
Mohit Kumar <mk2174334@gmail.com>
2026-09-04 09:34:28 +0530

    Control Statement

---
+++ practice1.py
@@ -0,0 +1,25 @@
+age = 20
+
+if age >= 18:
+    print("You are eligible to vote")
+
+
+age = 16
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible to vote")
+
+marks = 75
+
+if marks >= 90:
+    print("Grade A+")
+elif marks >= 75:
+    print("Grade A")
+elif marks >= 60:
+    print("Grade B")
+elif marks >= 40:
+    print("Grade C")
+else:
+    print("Fail")
