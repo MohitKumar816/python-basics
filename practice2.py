09a74b35dbc582ec99b314a4a9a55ef30d7f5f8e
Mohit Kumar <mk2174334@gmail.com>
2026-09-05 23:34:17 +0530

    Practice2.py

---
+++ practice2.py
@@ -0,0 +1,10 @@
+age = 20
+citizen = True
+
+if age >= 18:
+    if citizen == True:
+        print("You can vote")
+    else:
+        print("You are not a citizen")
+else:
+    print("You are under age")
