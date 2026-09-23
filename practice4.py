d44c8717092bc95f582a94a10bf91915ab1c3f43
Mohit Kumar <mk2174334@gmail.com>
2026-09-23 09:19:28 +0530

    Practice4

---
+++ practice4.py
@@ -0,0 +1,43 @@
+print("Hello Mohit")
+
+if 5 > 2:
+    print("True")
+
+x = 10 
+print(id(x))
+
+y = 40
+print(type(x))
+
+a = "Delhi"
+print(type(a))
+
+c = 45.7
+print(type(c))
+
+d = [4,6]
+print(type(d))
+
+e = (8,4)
+print(type(e))
+
+f = {1,2}
+print(type(f))
+
+name = "Mohit"
+print(name)
+
+name = input("Enter name:")
+print(name)
+
+age = input("Enter age:")
+print(age)
+
+course = input("Enter Course:")
+print(course)
+
+
+
+
+
+
